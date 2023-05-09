a0985e37e78dd11665df0005ecaba003aecb514f
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2023-05-10 01:28:05 +0900

    add CLASSIFIERS in setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Markup",
     ],
     python_requires='>=3.7',
 )
